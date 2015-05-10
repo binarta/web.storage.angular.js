f330f82d3c9048cc1dbc53058e598cebbbccf758
Jo Kerkhofs <jke@thinkerit.be>
2015-05-10 15:39:15 +0200

    When sessionStorage or localStorage cannot be used, fallback to in-memory storage

--- src/web.storage.js
+++ src/web.storage.js
@@ -1,3 +1,18 @@
 angular.module('web.storage', [])
-    .factory('localStorage', function() {return localStorage;})
-    .factory('sessionStorage', function() {return sessionStorage;});
+    .factory('localStorage', function() {
+        var local = {};
+        try {
+            localStorage.storageAvailable = true;
+            return localStorage;
+        } catch (e) {
+            return local;
+        }
+    }).factory('sessionStorage', function() {
+        var session = {};
+        try {
+            sessionStorage.storageAvailable = true;
+            return sessionStorage;
+        } catch (e) {
+            return session;
+        }
+    });
