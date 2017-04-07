981e1ae49f81ec8ca954d5177b48d659ab283715
Jo Kerkhofs <jke@thinkerit.be>
2017-04-07 10:40:54 +0200

    Extracted save retrieval of web storage to generic module.

--- src/web.storage.js
+++ src/web.storage.js
@@ -4,35 +4,10 @@
         .factory('sessionStorage', SessionStorageFactory);
 
     function LocalStorageFactory() {
-        return StorageFactory('localStorage');
+        return WebStorageFactory('localStorage');
     }
 
     function SessionStorageFactory() {
-        return StorageFactory('sessionStorage');
-    }
-
-    function StorageFactory(storage) {
-        try {
-            window[storage].setItem('storageAvailable', true);
-            return window[storage];
-        } catch (ignored) {
-            return new InMemoryStorage(storage);
-        }
-    }
-
-    function InMemoryStorage(id) {
-        var self = this;
-
-        this.setItem = function (k, v) {
-            self[k] = '' + v
-        };
-
-        this.getItem = function (k) {
-            return self[k] || null;
-        };
-
-        this.removeItem = function (k) {
-            delete self[k];
-        }
+        return WebStorageFactory('sessionStorage');
     }
 })();
